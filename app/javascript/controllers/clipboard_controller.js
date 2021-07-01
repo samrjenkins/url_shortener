92e354132dac57f76f952a35d6f1388a5770aade
Sam Jenkins <sam.jenko@hotmail.com>
2021-07-02 00:13:24 +0100

    add clipboard JS

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,18 +1,30 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
 
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "clip", "trigger" ]
 
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+  copy() {
+    this._copyToClipboard()
+    this._changeTriggerText(this.toggleText)
+    setTimeout(this._changeTriggerText.bind(this), 3000, this.textCache)
+  }
+
+  _copyToClipboard() {
+    const range = document.createRange();
+    range.selectNode(this.clipTarget);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  }
+
+  _changeTriggerText(newText) {
+    this.textCache = this.triggerTarget.textContent
+    this.triggerTarget.textContent = newText
+  }
+
+  get toggleText() {
+    return this.triggerTarget.dataset.toggleText
   }
 }
