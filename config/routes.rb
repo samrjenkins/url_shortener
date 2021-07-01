ea9a5289f4ffaab0415f03698b452453e9ec609f
Sam Jenkins <sam.jenko@hotmail.com>
2021-07-01 19:38:49 +0100

    remove redundant home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+  root to: 'shortened_urls#index'
   resources :shortened_urls, only: %i[index new create destroy]
   constraints subdomain: 's' do
     resources :shortened_urls, path: '', only: :show, module: :shortener
